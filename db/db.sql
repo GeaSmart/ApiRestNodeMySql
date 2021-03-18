e1961542e1b97986c397a92ca9d4767f4970d510
Gerson Azabache Martinez <gerson_dunk@hotmail.com>
2021-03-18 12:17:25 -0500

    stored procedure script added in db.sql file

--- db/db.sql
+++ db/db.sql
@@ -9,3 +9,24 @@ create table projects(
 );
 
 describe projects;
+
+DELIMITER //
+create procedure projectAddOrEdit(
+	in _id int,
+	in _name varchar(45),
+	in _budget int
+)
+begin
+	IF _id = 0 then    
+		insert into projects (name, budget) values (_name, _budget);
+        set _id = last_insert_id();        
+    ELSE
+		update projects
+        set name = _name,
+        budget = _budget
+        where id = _id;
+	END IF;
+    select _id as id;
+end
+//
+DELIMITER ;
